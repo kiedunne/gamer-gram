f7c62d6eccfca70de1f4349e516c365c4299f1a9
kiedunne <kie.dunne@gmail.com>
2018-12-28 18:09:54 -0500

    added post controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  get 'post/index'
+  get 'post/new'
+  get 'post/create'
+  get 'post/show'
+  get 'post/update'
+  get 'post/destroy'
   get 'home/index'
+  root 'home#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
