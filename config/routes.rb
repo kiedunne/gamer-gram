a5e076c23be347324c471480df14b9a2b887276b
kiedunne <kie.dunne@gmail.com>
2018-12-29 13:16:53 -0500

    Edit user profile

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
-resources :post
+resources :posts
+resources :users, only: [:show, :edit, :update]
   get 'home/index'
   root 'home#index'
+
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
---
+++ db/migrate/20181229175655_add_more_fields_to_users.rb
@@ -0,0 +1,5 @@
+class AddMoreFieldsToUsers < ActiveRecord::Migration[5.2]
+  def change
+    add_column :users, :bio, :text
+  end
+end
