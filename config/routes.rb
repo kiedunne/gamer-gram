a3558a427162477d94379b03e42fcc1e446080a4
kiedunne <kie.dunne@gmail.com>
2018-12-29 15:31:26 -0500

    Post image upload working

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
-  devise_for :users
-resources :posts
-resources :users, only: [:show, :edit, :update]
-  get 'home/index'
-  root 'home#index'
+  Rails.application.routes.draw do
+    root to: 'home#index'
 
-
+    devise_for :users
+    resources :users, only: [:show, :edit, :update]
+    resources :posts, only: [:new, :create, :show, :destroy]
+    get 'search' => 'search#index'
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
