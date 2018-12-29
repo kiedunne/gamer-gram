ba1d992bbbfb20aded34a5f77439eb06079aa848
kiedunne <kie.dunne@gmail.com>
2018-12-29 16:39:25 -0500

    Added username auth to signup

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,12 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  has_many :posts, dependent: :destroy 
+  has_many :posts, dependent: :destroy
+  validates_uniqueness_of :username, :email
+
+  attr_writer :login
+
+  def login
+    @login || self.username || self.email
+  end
 end
