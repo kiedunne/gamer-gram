d321d6765eb505949a7724c74c2e531162e84720
kiedunne <kie.dunne@gmail.com>
2018-12-29 20:07:19 -0500

    fixed profile link bug

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
 
   def show
     @user  = User.find(params[:id])
-    @posts = current_user.posts.order(created_at: :desc)
+    @posts = @user.posts.order(created_at: :desc)
   end
 
   def edit
